6e36ba60cde011357668475060d1e509633bfe62
NewFrontendDeveloper <den-tarakanov@inbox.ru>
2024-09-18 18:59:49 +0300

    swiperJS

---
+++ blocks/image-slider/image-slider.js
@@ -0,0 +1,11 @@
+new Swiper('.image-slider', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+    }
+})
